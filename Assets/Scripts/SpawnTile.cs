38d05f48ba6916a44149e451d760aaa72442006e
nikhilmsharma <77634325+ni-ms@users.noreply.github.com>
2022-07-29 16:52:33 +0530

    Added files

--- Assets/Scripts/SpawnTile.cs
+++ Assets/Scripts/SpawnTile.cs
@@ -5,41 +5,49 @@ using UnityEngine;
 public class SpawnTile : MonoBehaviour
 {
     public GameObject tileToSpawn;
-    public GameObject prevTile;
-    public float distanceBetweenTiles = 0f;
-    public float randomVal = 0.8f;
-    public float timeOffset = 0.4f;
+    public GameObject tileToSpawnTurn;
+    public GameObject referenceObject;
+    public float timeOffset = 0.01f;
+    public float distanceBetweenTiles = 0.0F;
+    public float randomValue = 0.5f;
     private Vector3 previousTilePosition;
     private float startTime;
-    private Vector3 direction, mainDirection = new Vector3(0, 0, 1), nextDirection = new Vector3(1, 0, 0); 
+    private Vector3 direction, mainDirection = new Vector3(0, 0, 1), otherDirection = new Vector3(1, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        previousTilePosition = prevTile.transform.position;
+        previousTilePosition = referenceObject.transform.position;
         startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time - startTime > timeOffset)
+        if (Time.time - startTime > timeOffset)
         {
-            if(Random.value < randomVal)
-            {
+            if (Random.value < randomValue)
                 direction = mainDirection;
-            }
             else
             {
                 Vector3 temp = direction;
-                direction = nextDirection;
+                direction = otherDirection;
                 mainDirection = direction;
-                nextDirection = temp;
+                otherDirection = temp;
             }
-            Vector3 spawnPos = previousTilePosition + distanceBetweenTiles * direction;
+            Vector3 spawnPos = previousTilePosition + distanceBetweenTiles * direction * 50;
             startTime = Time.time;
+            if(direction == new Vector3(0,0,1))
             Instantiate(tileToSpawn, spawnPos, Quaternion.Euler(0, 0, 0));
+            else if(direction == new Vector3(0, 1, 0) || direction == new Vector3(1, 0, 0))
+            {
+                
+                Instantiate(tileToSpawnTurn, spawnPos, Quaternion.Euler(0, 270, 0));
+                if(direction == previousTilePosition)
+                Instantiate(tileToSpawn, spawnPos, Quaternion.Euler(0, 0, 0));
+            }
+            
             previousTilePosition = spawnPos;
-        }
-        
+        } 
     }
 }

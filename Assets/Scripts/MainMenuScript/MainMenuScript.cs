3fed4f4997e3fd73c49f9142c694bcf0a7854b15
nikhilmsharma <77634325+ni-ms@users.noreply.github.com>
2022-07-29 17:18:44 +0530

    Fixed the main menu

--- Assets/Scripts/MainMenuScript/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript/MainMenuScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
 
     public void PlayGame()

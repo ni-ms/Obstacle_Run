f2ad7959993facb9aea21afcc1f446a68da0618f
nikhilmsharma <77634325+ni-ms@users.noreply.github.com>
2022-07-30 15:30:00 +0530

    Added Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     
     public float moveSpeed = 5;
     public Rigidbody playerBody;
 
+    float horizontalInput;
+    float horizontalSpeed = 2;
     private void FixedUpdate()
     {
         Vector3 moveForward = transform.forward * moveSpeed * Time.fixedDeltaTime;
-        playerBody.MovePosition(playerBody.position + moveForward);
+        Vector3 horizontalMove = horizontalSpeed * transform.right * horizontalInput * moveSpeed * Time.fixedDeltaTime;
+        playerBody.MovePosition(playerBody.position + moveForward + horizontalMove);
     }
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,6 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");   
     }
 }
